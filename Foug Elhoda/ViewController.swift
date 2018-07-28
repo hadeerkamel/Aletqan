7b1f1ae992bbde825f9d3eccbda273a668ee1ce9
Hadeer Kamel <hadeer@Hadeers-Mac.local>
2018-07-28 08:34:53 -0700

    Initial Commit

---
+++ Foug Elhoda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foug Elhoda
+//
+//  Created by Hadeer Kamel on 7/28/18.
+//  Copyright © 2018 Hadeer Kamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
